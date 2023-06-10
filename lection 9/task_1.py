6d064c0077b6b2fb4fbb850640022a927fb7a5a0
RumyantsevP <132554902+RumyantsevP@users.noreply.github.com>
2023-06-10 21:33:32 +0300

    Update task_1.py
    Сделал через "with"

--- lection 9/task_1.py
+++ lection 9/task_1.py
@@ -5,15 +5,15 @@
 
 # Здесь пишем код
 a = '0123456789'  # Строка из символов, которые нужно удалить
-in_old_file = open(r'C:\Users\pn.rumyancev\Tensor_autotest_course\lection 9\test_file\task1_data.txt', encoding='utf-8')  # В режиме чтения
-in_new_file = open(r'C:\Users\pn.rumyancev\Tensor_autotest_course\lection 9\test_file\task1_answer.txt', mode='a', encoding='utf-8')  # В режиме добавления
-list_row = in_old_file.readlines()  # Список из строк исходного текста из файла "task1_data.txt"
-for i in list_row:
-    new_str = ''
-    for j in i:
-        if j not in a:
-            new_str += j
-    in_new_file.write(new_str)  # Записываем измененный текст в файл "task1_answer.txt"
+with open(r'C:\Users\pn.rumyancev\Tensor_autotest_course\lection 9\test_file\task1_data.txt', encoding='utf-8') as in_old_file: # В режиме чтения
+    with open(r'C:\Users\pn.rumyancev\Tensor_autotest_course\lection 9\test_file\task1_answer.txt', mode='w', encoding='utf-8') as in_new_file:  # В режиме записи
+        list_row = in_old_file.readlines()  # Список из строк исходного текста из файла "task1_data.txt"
+        for i in list_row:
+            new_str = ''
+            for j in i:
+                if j not in a:
+                    new_str += j
+            in_new_file.write(new_str)  # Записываем измененный текст в файл "task1_answer.txt"
 
 
 # Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
